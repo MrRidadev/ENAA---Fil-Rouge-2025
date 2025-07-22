6743edb2bb1915ef5086c8bcd0e810b7980094ef
ridaDev <kiratterida@gmail.com>
2025-07-22 15:03:27 +0100

    modifer code et créer test pour ajouter et afficher

--- src/test/java/org/example/backend/Services/FilmServiceTest.java
+++ src/test/java/org/example/backend/Services/FilmServiceTest.java
@@ -3,14 +3,13 @@ package org.example.backend.Services;
 import org.example.backend.entity.Film;
 import org.example.backend.repository.FilmRepository;
 import org.example.backend.service.FilmService;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 
 @SpringBootTest
@@ -23,26 +22,20 @@ public class FilmServiceTest {
     @Autowired
     private FilmRepository filmRepository;
 
-    @BeforeEach
-    void setUp() {
-        filmRepository.deleteAll(); // Nettoyage de la BDD entre les tests
-    }
-
     @Test
-    void testAddFilm() {
-        // Arrange
+    public void testAddAndGetFilm() {
         Film film = new Film();
         film.setTitre("Inception");
-        film.setGenre("Science Fiction");
-        film.setDescription("Un thriller psychologique.");
-        film.setLangue("Anglais");
+        film.setGenre("Sci-fi");
+        film.setDescription("A mind-bending thriller");
+        film.setLangue("English");
 
-        // Act
-        Film savedFilm = filmService.addFilm(film);
+        Film saved = filmService.addFilm(film);
 
-        // Assert
-        assertNotNull(savedFilm.getIdFilm());
-        assertEquals("Inception", savedFilm.getTitre());
+        Assertions.assertNotNull(saved.getIdFilm());
+        Assertions.assertEquals("Inception", saved.getTitre());
     }
+
+
 
 }
