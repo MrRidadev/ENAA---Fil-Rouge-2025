b7bd92642dfc68121fab2a5407c52d31c450f036
ridaDev <kiratterida@gmail.com>
2025-09-09 17:23:18 +0100

    ajouter function count client

--- src/main/java/org/example/backend/repository/ClientRepository.java
+++ src/main/java/org/example/backend/repository/ClientRepository.java
@@ -2,10 +2,13 @@ package org.example.backend.repository;
 
 import org.example.backend.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ClientRepository   extends JpaRepository<Client, Long> {
 
     Optional<Client> findByEmail(String email);
+    @Query(value = "select count(*) from client",nativeQuery = true)
+    int countClient();
 }
