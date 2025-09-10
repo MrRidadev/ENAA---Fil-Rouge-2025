d611caacf081f542ae4ab2963dbe72ed9d5f3656
ridaDev <kiratterida@gmail.com>
2025-09-10 17:21:11 +0100

    crée function find film by titre et genre

--- src/main/java/org/example/backend/repository/FilmRepository.java
+++ src/main/java/org/example/backend/repository/FilmRepository.java
@@ -4,9 +4,15 @@ import org.example.backend.entity.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface FilmRepository extends JpaRepository<Film, Long> {
 
     //count client
     @Query(value = "select count(*) from film",nativeQuery = true)
     int countFilms();
+
+    List<Film> findFilmByTitre(String titre);
+
+    List<Film> findByGenre(String genre);
 }
